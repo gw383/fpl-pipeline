72b3b94d9ebc421226cf476d3fbd96033c45d013
unknown <georgewilliams383@gmail.com>
2026-06-17 16:56:50 +0100

    added query for new misc team data

---
+++ misc_sql/team_misc.sql
@@ -0,0 +1,27 @@
+create table analytics.team_misc
+(tm_id int not null,
+ tm_primary varchar(7) null,
+ tm_secondary varchar(7) null);
+
+
+insert into analytics.team_misc values
+(1, '#EF0107', '#FFFFFF'),
+(2, '#95BFE5', '#670E36'),
+(4, '#DA291C', '#000000'),
+(5, '#E30613', '#FFFFFF'),
+(6, '#0057B8', '#FFFFFF'),
+(7, '#034694', '#FFFFFF'),
+(8, '#1B458F', '#C4122E'),
+(9, '#003399', '#FFFFFF'),
+(10, '#FFFFFF', '#000000'),
+(11, '#FFCD00', '#1D428A'),
+(12, '#C8102E', '#00B2A9'),
+(13, '#6CABDD', '#1C2C5B'),
+(14, '#DA291C', '#FBE122'),
+(15, '#241F20', '#FFFFFF'),
+(16, '#DD0000', '#FFFFFF'),
+(17, '#EB172B', '#FFFFFF'),
+(18, '#132257', '#FFFFFF'),
+(3, '#6C1D45', '#99D6EA'),
+(19, '#7A263A', '#1BB1E7'),
+(20, '#FDB913', '#231F20');
